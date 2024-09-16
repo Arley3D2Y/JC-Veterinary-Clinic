bbc4163629876284255727cd140372cbf2600d12
Arley Bernal <arleybernal0418@gmail.com>
2024-09-16 10:50:52 -0500

    Creación de landing page usando componentes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderHomeComponent } from './landing-page/header-home/header-home.component';
+import { ServicesHomeComponent } from './landing-page/services-home/services-home.component';
+import { AboutHomeComponent } from './landing-page/about-home/about-home.component';
+import { ContactHomeComponent } from './landing-page/contact-home/contact-home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, HeaderHomeComponent, ServicesHomeComponent, AboutHomeComponent, ContactHomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
