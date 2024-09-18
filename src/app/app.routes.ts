6ce2567c5279618075cf1a7837418795c96e5ad5
Arley Bernal <arleybernal0418@gmail.com>
2024-09-18 14:34:56 -0500

    Creación de componente entity-overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginContainerComponent } from './login/login-container/login-container.component';
 import { CustomersSearchComponent } from './veterinary/customers-search/customers-search.component';
-
+import { EntityOverviewComponent } from './veterinary/entity-overview/entity-overview.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirección a home
     { path: 'home', component: LandingPageComponent }, // Ruta para la landing page
     { path: 'login', component:  LoginContainerComponent}, // Login usuario
-    { path: 'veterinario/clientes',
-        component: CustomersSearchComponent,
+    { path: 'veterinario', component: CustomersSearchComponent,
         children: [
           { path: '', redirectTo: 'clientes', pathMatch: 'full' },  // Redirige a 'clientes' al entrar a 'veterinario'
+          { path: 'detalles-cliente/:id', component: EntityOverviewComponent }
         ]
       },
     { path: '**', redirectTo: '/home' } // Redirección en caso de ruta no encontrada
--- src/app/veterinary/entity-overview/entity-overview.component.ts
+++ src/app/veterinary/entity-overview/entity-overview.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Customer } from '../../model/customer';
+import { CustomerService } from '../../services/customer.service';
+import { SharedHeaderComponent } from '../shared-header/shared-header.component';
 
 @Component({
   selector: 'app-entity-overview',
   standalone: true,
-  imports: [],
+  imports: [
+    SharedHeaderComponent
+  ],
   templateUrl: './entity-overview.component.html',
   styleUrl: './entity-overview.component.css'
 })
 export class EntityOverviewComponent {
+  @Input()
+  customerSelected!: Customer;
 
+  constructor(
+    private route: ActivatedRoute,
+    private customerService: CustomerService  
+  ) {
+  }
+
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = Number(params.get('id'));
+      this.customerSelected = this.customerService.findById(id);
+    });
+  }
 }
